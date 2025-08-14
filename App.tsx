b672b3b234d9f60c1f9a3cd8f6927c693d302d94
JoaoVFMR <1051392411009@fatec>
2025-08-14 08:48:13 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Olá Mundo!</Text>
+        <Button mode='outlined' icon='camera'>
+          Clique aqui
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
